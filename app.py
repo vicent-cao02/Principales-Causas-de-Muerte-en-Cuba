19d31e1ace529f4ce48cee843a83391696fe3a51
Vicente Cao <vicentecaotarrero@gmail.com>
2024-08-15 23:38:22 +0200

    google analytics

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit.components.v1 as components
+
+GA_TRACKING_ID = 'G-F0GP02LFKX'  
+
+ga_code = f"""
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={GA_TRACKING_ID}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){{dataLayer.push(arguments);}}
+  gtag('js', new Date());
+  gtag('config', '{GA_TRACKING_ID}');
+</script>
+"""
+
+components.html(ga_code, height=0, width=0)
 
 st.title('Principales Causas de Muerte en Cuba')
 
@@ -45,15 +61,49 @@ muertes_2022 = df_2022['Número de Muertes']
 
 st.title('Prevenir el riesgo de morir por Enfermedades No Transmisibles(ENT)')
 st.write("Las Enfermedades No Transmisibles (ENT) constituyen seis de las principales causas de mortalidad en Cuba. Estas incluyen tumores malignos, enfermedades cerebrovasculares, diabetes, cirrosis, enfermedades crónicas respiratorias y enfermedades de las arterias. En 2022, estas enfermedades causaron un total de 423,3 muertes por cada 100,000 habitantes, representando el 40% de las principales causas de muerte en el país. Esta alta tasa de mortalidad destaca la magnitud del problema y subraya la necesidad urgente de estrategias de prevención y tratamiento efectivas.")
-st.write("Según un artículo publicado por el Ministerio de Salud Pública de Cuba (MINSAP) en febrero de 2020, la prevención de la muerte prematura causada por enfermedades no transmisibles (ENT) en Cuba se enfrenta a varios factores de riesgo significativos. Estos incluyen el tabaquismo y la exposición al humo de tabaco, una dieta poco saludable, la inactividad física, la obesidad y el consumo excesivo de alcohol. Recientemente, se ha añadido la contaminación del aire a esta lista, reconociéndose a nivel mundial como un factor de riesgo importante para las enfermedades cardiovasculares y respiratorias crónicas. Abordar estos factores es crucial para mejorar la salud pública y reducir la mortalidad prematura asociada con las ENT.")
-st.write("Las condiciones de vida, el empleo, el ambiente laboral, la educación, la globalización, así como las situaciones económicas y demográficas y la urbanización, son factores clave que determinan la prevalencia de enfermedades no transmisibles (ENT). Estos desafíos requieren un esfuerzo concertado tanto de los servicios de salud como de toda la sociedad, dado que se trata de un problema que afecta a nivel global, no solo a Cuba..")
-
 fig1 = go.Figure(data=[go.Bar(x=enfermedades, y=muertes_2022, marker_color='blue')])
 
 fig1.update_layout(
-    title='Número de Muertes en 2022 por Enfermedad No Transmisibles',
+    title='Número de Muertes en 2022 por Enfermedad No Transmisibles(ENT)',
     xaxis_title='Enfermedades',
     yaxis_title='Número de Muertes',
     xaxis_tickangle=-45  
 )
 st.plotly_chart(fig1)
+st.write("Según un artículo publicado por el Ministerio de Salud Pública de Cuba (MINSAP) en febrero de 2020, la prevención de la muerte prematura causada por enfermedades no transmisibles (ENT) en Cuba se enfrenta a varios factores de riesgo significativos. Estos incluyen el tabaquismo y la exposición al humo de tabaco, una dieta poco saludable, la inactividad física, la obesidad y el consumo excesivo de alcohol. Recientemente, se ha añadido la contaminación del aire a esta lista, reconociéndose a nivel mundial como un factor de riesgo importante para las enfermedades cardiovasculares y respiratorias crónicas. Abordar estos factores es crucial para mejorar la salud pública y reducir la mortalidad prematura asociada con las ENT.")
+st.write("Las condiciones de vida, el empleo, el ambiente laboral, la educación, la globalización, así como las situaciones económicas y demográficas y la urbanización, son factores clave que determinan la prevalencia de enfermedades no transmisibles (ENT). Estos desafíos requieren un esfuerzo concertado tanto de los servicios de salud como de toda la sociedad, dado que se trata de un problema que afecta a nivel global, no solo a Cuba..")
+
+
+st.title("Disparidades de Género")
+st.write("")
+
+def load_data():
+    return pd.read_json('data/output1.json')
+data1 = load_data()
+
+df1 = pd.DataFrame(data1)
+excluir_enfermedades = ["COVID-19", "Accidentes ","Influenza y neumonia","Lesiones autoinflingidas accidentalmente","Influenza y neumonia ","Lesiones autoinfligidas intencionalmente "]
+df1 = df1[~df1['Enfermedades'].isin(excluir_enfermedades)]
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    x=df1['Enfermedades'],
+    y=df1['Tasas masculinas '],
+    name='Tasas masculinas',
+    marker_color='blue'
+))
+
+fig.add_trace(go.Bar(
+    x=df1['Enfermedades'],
+    y=df1['Tasas femeninas'],
+    name='Tasas femeninas',
+    marker_color='#D81B60'
+))
+
+fig.update_layout(
+    title='Tasas de mortalidad por 100,000 habitantes por Enfermedad y Sexo en 2022',
+    xaxis_title='Enfermedades',
+    yaxis_title='Número de Tasas de mortalidad',
+    barmode='group'
+)
+st.plotly_chart(fig)
