d3d27509911ff00524bf73fb30d5de326b324eea
Vicente Cao <vicentecaotarrero@gmail.com>
2024-09-02 04:41:51 +0200

    Update _📃_Presentacion.py

--- _📃_Presentacion.py
+++ _📃_Presentacion.py
@@ -1,1 +1,7 @@
 import streamlit as st
+import streamlit_analytics 
+import json
+import matplotlib.pyplot as plt
+
+streamlit_analytics.track(save_to_json = "./data/metrics.json" ) 
+streamlit_analytics.track(load_from_json="./data/metrics.json")
