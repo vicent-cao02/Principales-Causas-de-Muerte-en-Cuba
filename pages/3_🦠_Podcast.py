2e8ee69f4523c690428a8a25ef77332623ade882
Vicente Cao <vicentecaotarrero@gmail.com>
2024-09-02 06:45:05 +0200

    update

---
+++ pages/3_🦠_Podcast.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import streamlit_analytics
+
+st.title("Podcast: La Mortalidad en Cuba")
+
+with streamlit_analytics.track():
+
+    
+    def likes():
+        st.subheader("¿Te gusta este artículo?")
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            if st.button("👍 Me gusta"):
+                st.success("¡Gracias por tu feedback positivo!")
+
+        with col2:
+            if st.button("👎 No me gusta"):
+                st.error("¡Gracias por tu feedback negativo!")
+
+    likes()
+    def comentarios():
+        with st.form(key='comment_form'):
+            comment = st.text_area("Escribe tu comentario:")
+            submit_button = st.form_submit_button("Enviar Comentario")
+
+            if submit_button:
+                if comment:
+                    st.success("¡Gracias por tu comentario!")
+                else:
+                    st.error("Por favor, escribe un comentario antes de enviar.")
+
+    comentarios()
